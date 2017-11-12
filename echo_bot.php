c5c609ae40162b4065f84295dcbaa6b928eb3f11
Candra Gunawan <tatareload5758@gmail.com>
2017-11-13 03:40:25 +0700

    Add files via upload

---
+++ echo_bot.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once('./LINEBotTiny.php');
+
+$channelAccessToken = 'GjpNINkqOZTxwSS+Fb8ASd6BaobIx7zV3SGGUDM4ynFQBXYr7e0KY1iYV2n+JGpzsFpHb+65AsH/CliRXoHkRrjYJmEatwNA97/aIm8FHCVgy6IA7roRWd3cXrEywyMgIjHBBhoU9fbULH35qcbKvwdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'a8547205e99263f1808089c9911c0292';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
